51958ba204baed800da106b67b273c350e468b96
molinell <molinell@arcada.fi>
2024-10-22 14:20:35 +0300

    Version 1

--- quizGame/src/main/java/org/irel/Player.java
+++ quizGame/src/main/java/org/irel/Player.java
@@ -4,15 +4,20 @@ import java.util.List;
 
 public class Player {
 
-private String name;
-private int age;
-private String password;
+    private String name;
+    private int age;
+    private String password;
 
 
-public Player (String name, int age, String password) {
-    this.name = name;
-	this.age = age;
-    this.password = password;
-	}
+    public Player (String name, int age, String password) {
+        this.name = name;
+	    this.age = age;
+        this.password = password;
+    }
+
+    public Player (String name, int age){
+        this.name = name;
+        this.age = age;
+    }
     
 }
