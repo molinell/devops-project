c36c3a6249dd451b71fd0f2e4b0a7a506ff997be
molinell <molinell@arcada.fi>
2024-10-24 12:14:37 +0300

    V3 login

--- quizGame/src/main/java/org/irel/GameV3.java
+++ quizGame/src/main/java/org/irel/GameV3.java
@@ -1,5 +1,12 @@
 package org.irel;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
@@ -9,15 +16,45 @@ public class GameV3 {
 	// Select categories (done)
 	// Choose difficulties 
 	// Leader board of all players
-	
-	private int rounds;
-	private Player player;
 	private ArrayList<Question> questions;
+	private DB db = new DB();
+	private ArrayList<Player> users;
+	private Player player;
+	private boolean loggedIn = false;
+	private int rounds;
+	private boolean loginComplete = false;
+	private HttpServer server;
+	Question.Categories selectedCategory;
 
 	GameV3() {
 		// Same code as earlier
-		generateQuestions();
+		questions = Question.generateQuestions();
+		this.users = db.getUsers();
+		this.player = null;
+
+		if(!login()) {
+			System.out.println("An error occured, try again later");
+			System.exit(1);
+		}
+
+		//https://stackoverflow.com/questions/21124879/how-do-i-make-java-wait-for-a-method-to-finish-before-continuing
+		//https://www.baeldung.com/java-wait-notify
+		synchronized (this) {
+			while (!loginComplete) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		server.stop(0);
+
+		if(!loggedIn) System.exit(1);
+
 		Scanner scanner = new Scanner(System.in);
+
+
 		System.out.print("How many rounds of questions?: ");
 		String roundsString = scanner.nextLine();
 		int r = Integer.parseInt(roundsString);
@@ -27,34 +64,20 @@ public class GameV3 {
 
 		System.out.println("*** Welcome to quiz game ***");
 
-		System.out.print("Enter your name: ");
-		String name = scanner.nextLine();
-
-		System.out.print("Enter your age: ");
-		int age = scanner.nextInt();
-
-		if (age < 16) {
-			System.out.println("ERROR: You're too young to play");
-			System.exit(1);
-		}
-
-		this.player = new Player(name, age);
-
 		// Version 2 code with category
 		System.out.println("\nPlease select a category:");
 		for (Question.Categories category : Question.Categories.values()) {
 			System.out.println(category.ordinal() + 1 + ". " + category);
 		}
 		int categorySelection = scanner.nextInt();
-		Question.Categories selectedCategory = Question.Categories.values()[categorySelection - 1];
+		this.selectedCategory = Question.Categories.values()[categorySelection - 1];
 		scanner.nextLine();
 
 		System.out.println("\nYou selected: " + selectedCategory + "\n");
 
-		run(selectedCategory);
 	}
 
-	public void run(Question.Categories selectedCategory) {
+	public void run() {
 		int points = 0;
 		Scanner scanner = new Scanner(System.in);
 		// ArrayList<Question> unUsedQ = questions;
@@ -104,72 +127,87 @@ public class GameV3 {
 		System.out.printf("You got %d / %d points\n", points, rounds);
 	}
 
-	private void generateQuestions() {
-		questions = new ArrayList<>();
-
-		questions.add(new Question(Question.Categories.POLITICS, "Who is the current president of finland?", 0,
-				new String[] { "Alexander Stubb", "Petteri Orpo", "Sauli Niinistö", "Donald Duck" }));
-		questions.add(new Question(Question.Categories.POLITICS, "Who is the PM party of finland", 1,
-				new String[] { "SDP", "Kok.", "PS", "Kesk." }));
-		questions.add(new Question(Question.Categories.POLITICS, "Which percentage of alcohol can you buy in store?", 3,
-				new String[] { "6%", "5,5%", "8,5%", "8%" }));
-		questions.add(new Question(Question.Categories.POLITICS,
-				"Which US state considers that pickles should bounce to be safe for human consumption", 0,
-				new String[] { "Connecticut", "New York", "New Hampshire", "Pennsylvania" }));
-		questions.add(new Question(Question.Categories.POLITICS, "Which italian politician dissed finnish food?", 1,
-				new String[] { "Antonio Mazzone", "Silvio Berlusconi", "Lucio Magri", "Ernesto Nathan" }));
-
-		questions.add(new Question(Question.Categories.FOOD, "Which of the following is not a grain?", 2,
-				new String[] { "chia", "spelt", "potatoes", "peanuts" }));
-		questions.add(new Question(Question.Categories.FOOD,
-				"What fruit is known as 'the king of fruits' in many south asian countries?", 1,
-				new String[] { "Mango", "Durian", "Pineapple", "Papaya" }));
-		questions.add(new Question(Question.Categories.FOOD, "Which spice is commonly used in Indian curry dishes", 2,
-				new String[] { "Basil", "Paprika", "Turmeric", "Rosemary" }));
-		questions.add(new Question(Question.Categories.FOOD,
-				"What fruit is known as 'the king of fruits' in many south asian countries?", 2,
-				new String[] { "Mango", "Durian", "Pineapple", "Papaya" }));
-		questions.add(
-				new Question(Question.Categories.FOOD, "Which of the following food is the netherlands NOT famous for?",
-						3, new String[] { "Apple pie", "Stroopwaffle", "Poffertjes", "Speculaas" }));
-		questions.add(new Question(Question.Categories.FOOD, "Which of the following is a traditional finnish food?", 0,
-				new String[] { "Lax soppa", "Janssons frestelse", "Smörrebröd", "Fårikål" }));
-
-		questions.add(new Question(Question.Categories.POP_CULTURE, "How old is Elon Musk", 0,
-				new String[] { "53", "40", "61", "57" }));
-		questions.add(new Question(Question.Categories.POP_CULTURE, "Who founded amazon?", 1,
-				new String[] { "Elon Musk", "Jeff Bezos", "Mark Zuckerberg", "Warren Buffet" }));
-		questions.add(new Question(Question.Categories.POP_CULTURE, "What was the original version of facebook?", 2,
-				new String[] { "TheFaceBook", "FaceSmash", "FaceMash", "Face Book" }));
-		questions.add(new Question(Question.Categories.POP_CULTURE, "What is Warren Buffets favorite restaurant?", 0,
-				new String[] { "Mac Donald's", "KFC", "Five guys", "Wendy's" }));
-		questions.add(new Question(Question.Categories.POP_CULTURE, "Who did Steve Jobs exploit?", 2,
-				new String[] { "Michal Solowow", "Robert Lewandowski", "Steve Wozniak", "Mikolaj Kopernik" }));
-
-		questions.add(new Question(Question.Categories.GEOGRAPHY, "Which of the following countries is not in europe?",
-				1, new String[] { "Cyprus", "Kazakhstan", "Poland", "England" }));
-		questions.add(new Question(Question.Categories.GEOGRAPHY, "What is the capital of India?", 0,
-				new String[] { "New Delhi", "Budaun Sadar", "Mumbai", "Nagpur" }));
-		questions.add(new Question(Question.Categories.GEOGRAPHY, "What is the capital of Egypt?", 3,
-				new String[] { "Alexandria", "Bani Mazar", "Mallawi", "Cairo" }));
-		questions.add(new Question(Question.Categories.GEOGRAPHY, "What is the capital of Mongolia?", 2,
-				new String[] { "Uliastai", "Sergelen", "Ulaanbaatar", "Altanbulag" }));
-		questions.add(new Question(Question.Categories.GEOGRAPHY, "What is the capital of Cote D'Ivore?", 0,
-				new String[] { "Abidjan", "Divo", "Man", "Korhogo" }));
-		questions.add(new Question(Question.Categories.GEOGRAPHY, "What is the capital of Brazil?", 2,
-				new String[] { "La Paz", "Asuncion", "Brasilia", "Manaus" }));
-
-		questions.add(new Question(Question.Categories.CONTROVERSY,
-				"Who is the most erratic person on X (formerly known as Twitter)", 4,
-				new String[] { "Donald Trump", "Andrew Tate", "Kanye West", "Elon Musk" }));
-		questions.add(new Question(Question.Categories.CONTROVERSY,
-				"Who of the following does not have allegations of sexual abuse (for now)", 1,
-				new String[] { "P Diddy", "Tom Hanks", "R. Kelly", "Jeffrey Epstein" }));
-		questions
-				.add(new Question(Question.Categories.CONTROVERSY, "What scandal happened 2015 involving Steve Harvey?",
-						0, new String[] { "He crowned the wrong miss universe", "Forgot his script in a SNL episode",
-								"Falsely rumored to be dead", "Twerked during the VMA" }));
+	private boolean login() {
+		try {
+			server = HttpServer.create(new InetSocketAddress(8080), 0);
+			server.createContext("/", new GameV3.FormHandler());
+			server.setExecutor(null); // creates a default executor
+			server.start();
+			System.out.println("Please navigate to the following link to login: http://localhost:8080/");
+			return true;
+		} catch (final IOException exception) {
+			exception.printStackTrace();
+			return false;
+		}
 	}
+
+	private class FormHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange exchange) throws IOException {
+			if ("GET".equals(exchange.getRequestMethod())) {
+				String response = "<html><head><style>"
+						+ "body {"
+						+ "    display: flex;"
+						+ "    justify-content: center;"
+						+ "    align-items: center;"
+						+ "    height: 100vh;"
+						+ "    margin: 0;"
+						+ "}"
+						+ "form {"
+						+ "    display: flex;"
+						+ "    flex-direction: column;"
+						+ "    align-items: center;"
+						+ "}"
+						+ "</style></head><body>"
+						+ "<form method='POST'>"
+						+ "    <label for='username'>Username:</label>"
+						+ "    <input type='text' id='username' name='username'><br><br>"
+						+ "    <label for='password'>Password:</label>"
+						+ "    <input type='password' id='password' name='password'><br><br>"
+						+ "    <input type='submit' value='Submit'>"
+						+ "</form></body></html>";
+
+				exchange.sendResponseHeaders(200, response.getBytes().length);
+				OutputStream os = exchange.getResponseBody();
+				os.write(response.getBytes());
+				os.close();
+			} else if ("POST".equals(exchange.getRequestMethod())) {
+				StringBuilder sb = new StringBuilder();
+				try (var is = exchange.getRequestBody()) {
+					int i;
+					while ((i = is.read()) != -1) {
+						sb.append((char) i);
+					}
+				}
+				String[] params = sb.toString().split("&");
+				String username = params[0].split("=")[1];
+				String password = params[1].split("=")[1];
+
+				String response = "<html><body><h1>Invalid username or password</h1><body></html>";
+				for(Player p : users){
+					if(p.getName().equals(username)){
+						if (p.getPassword().equals(password)) {
+							response = "<html><body><h1>Login successful, return to program to play</h1><body></html>";
+							loggedIn = true;
+							player = p;
+							break;
+						}
+					}
+				}
+
+				exchange.sendResponseHeaders(200, response.getBytes().length);
+				OutputStream os = exchange.getResponseBody();
+				os.write(response.getBytes());
+				os.close();
+
+				synchronized (GameV3.this) {
+					loginComplete = true;
+					GameV3.this.notify();
+				}
+			}
+		}
+	}
+
 
 	public int getGameRounds() {
 		return rounds;
